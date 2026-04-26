a16885ee402cccf8fc1402c7ffa551d80fd4648c
Noms Mdhluli <Noms.Mdhluli@eoh.com>
2026-04-26 21:06:56 +0200

    updated testimonial checks

--- checks/testimonialChecks.js
+++ checks/testimonialChecks.js
@@ -4,7 +4,7 @@ export function validateTestimonialResponse(response) {
     return check(response, {
         'status is 201':      (res) => res.status === 201,
         'body is not empty':  (res) => res.body.length > 0,
-        'has testimonial id': (res) => JSON.parse(res.body).data?.id !== undefined,
+        'has testimonial id': (res) => JSON.parse(res.body).data?.Id !== undefined,
     });
 }
 
@@ -12,8 +12,8 @@ export function validateUpdateTestimonialResponse(response) {
     return check(response, {
         'status is 200':        (res) => res.status === 200,
         'body is not empty':    (res) => res.body.length > 0,
-        'title was updated':    (res) => JSON.parse(res.body).data?.title === 'Updated Title',
-        'rating was updated':   (res) => JSON.parse(res.body).data?.rating === 5,
+        'title was updated':    (res) => JSON.parse(res.body).data?.Title === 'Updated Title',
+        'rating was updated':   (res) => JSON.parse(res.body).data?.Rating === 5,
     });
 }
 
